8de2bce972280882263f688153321175eebb750b
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-11-23 12:51:37 +0100

    Enhance track listening logic and implement posting of recently played tracks

--- src/main.py
+++ src/main.py
@@ -31,34 +31,29 @@ def main():
     client = Client()
     print(client)
     client.login(config['bluesky']['handle'], config['bluesky']['password'])
+    listened = []
     while True:
-        print("check")
-        # timeRemaining, currentTrack = get_now_playing(sp, currentTrack, config)
         track = sp.current_user_playing_track()     # Get track info
         print(track)
         if track and track['item']:
             duration = track['item']['duration_ms'] # in milliseconds
-            timeRemaining = max(0, duration - track['progress_ms'])/1000 # in seconds
-            print(timeRemaining)
-            nowPlaying = get_track_info(track)
-            if nowPlaying != currentTrack:
-                # post_now_playing(client, nowPlaying)
-                tb = client_utils.TextBuilder()
-                trackInfo = f"{nowPlaying['title']} by {nowPlaying['artist']}\n\n"
-                # tb.text(trackInfo)
-                tb.text("Now Playing: ")
-                tb.link(trackInfo, nowPlaying['url'])
-                # client.send_post(tb)
-                print(tb)
-                currentTrack = nowPlaying
-            else:
-                print("No change in track")
-                timeRemaining = max(0, duration - track['progress_ms'])/1000 # in seconds
-                print(timeRemaining)
-        else:
-            print("No track currently playing")
-            timeRemaining = 30
-        time.sleep(timeRemaining)
+            progress = track['progress_ms']
+            if progress < duration * 0.25:
+                currentTrack = track
+
+        if currentTrack and progress >= duration * 0.75:
+            trackInfo = get_track_info(currentTrack)
+            listened.append(trackInfo)
+            currentTrack = None
+
+            # if trackInfo and trackInfo['title'] not in listened:
+            #     listened.append(trackInfo['title'])
+            #     post_now_playing(client, trackInfo)
+        
+        if len(listened) >= 3:
+            post_tracks(client, listened)
+            listened = []
+        time.sleep(5)
 
 
 if __name__ == "__main__":
