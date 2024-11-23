8242b54851b312940cb0c59baa2b075b13f77816
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-11-23 14:03:48 +0100

    Implement track posting functionality and enhance error handling in main loop

--- src/bsky_utils.py
+++ src/bsky_utils.py
@@ -9,8 +9,6 @@ def post_now_playing(client, nowPlaying):
     """
     Post the current track to the Bluesky client
     """
-    # client = Client()
-    # client.login(config['bluesky']['handle'], config['bluesky']['password'])
 
     tb = client_utils.TextBuilder()
     tb.text = f"Now Playing: {nowPlaying['title']} by {nowPlaying['artist']}"
@@ -24,4 +22,38 @@ def post_now_playing(client, nowPlaying):
 
 
     # client.send_post(text=postText)
+    print(postText)
+
+
+def get_did_for_handle(client, handle):
+    """
+    Get the DID of a user by their handle
+    """
+    # client = Client()
+    # client.login(username, password)
+
+    response = client.com.atproto.identity.resolve_handle({'handle': handle})
+    did = response['did']
+    return did
+
+
+def post_tracks(client, tracks):
+    """
+    Post the current track to the Bluesky client
+    """
+    userDID = get_did_for_handle(client, 'whats-playing-bot.bsky.social')
+    tb = client_utils.TextBuilder()
+    tb.mention('@compscisi.bsky.social', userDID)
+    tb.text(f" recently listened to: : \n\n")
+    tb.link(f"{tracks[0]['title']} ", tracks[0]['url'])
+    tb.text(f"by {tracks[0]['artist']}, \n")
+    tb.link(f"{tracks[1]['title']} ", tracks[1]['url'])
+    tb.text(f"by {tracks[1]['artist']}, \n")
+    tb.link(f"{tracks[2]['title']} ", tracks[2]['url'])
+    tb.text(f"by {tracks[2]['artist']}\n")
+    
+
+    client.send_post(tb)
+
+    postText = f"Recently listened to: {tracks[0]['title']} by {tracks[0]['artist']}, {tracks[1]['title']} by {tracks[1]['artist']}, {tracks[2]['title']} by {tracks[2]['artist']}"
     print(postText)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+import re
 import time
+
+import spotipy
 
 from utils import get_config, init_spotipy
 # from spotify_utils import get_now_playing
 from atproto import Client, client_utils
-from utils import get_config
+from bsky_utils import post_now_playing, post_tracks
+
 
 # Load config
 config = get_config()
@@ -27,33 +31,41 @@ def main():
 
     sp = init_spotipy(config)
     currentTrack = None
-    timeRemaining = 30     # seconds
     client = Client()
     print(client)
     client.login(config['bluesky']['handle'], config['bluesky']['password'])
     listened = []
     while True:
-        track = sp.current_user_playing_track()     # Get track info
-        print(track)
-        if track and track['item']:
-            duration = track['item']['duration_ms'] # in milliseconds
-            progress = track['progress_ms']
-            if progress < duration * 0.25:
-                currentTrack = track
+        try:
+            track = sp.current_user_playing_track()     # Get track info
+            print(track)
+            if track and track['item']:
+                duration = track['item']['duration_ms'] # in milliseconds
+                progress = track['progress_ms']
+                if progress < duration * 0.25:
+                    currentTrack = track
+            
+            # ToDo: Should I prevent tracks being repeated?
+            if currentTrack and progress >= duration * 0.75:
+                trackInfo = get_track_info(currentTrack)
+                listened.append(trackInfo)
+                currentTrack = None
 
-        if currentTrack and progress >= duration * 0.75:
-            trackInfo = get_track_info(currentTrack)
-            listened.append(trackInfo)
-            currentTrack = None
+            
+            if len(listened) >= 3:
+                post_tracks(client, listened)
+                listened = []
 
-            # if trackInfo and trackInfo['title'] not in listened:
-            #     listened.append(trackInfo['title'])
-            #     post_now_playing(client, trackInfo)
-        
-        if len(listened) >= 3:
-            post_tracks(client, listened)
-            listened = []
-        time.sleep(5)
+            remainingTime = (duration - progress) / 1000    # in seconds
+            time.sleep(min(remainingTime, 10))
+        except spotipy.exceptions.SpotifyException as e:
+            if e.http_status == 429:
+                retryAfter = int(e.headers.get('Retry-After', 10))
+                print(f"Rate limited. Retrying in {retryAfter} seconds")
+                time.sleep(retryAfter)
+            else:
+                print(f"Error: {e}")
+                time.sleep(10)
 
 
 if __name__ == "__main__":
