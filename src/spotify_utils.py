801e420e15645265146b7d6a9727db4a428d30fd
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-11-23 17:14:40 +0100

    Comment out unused Spotify utility functions for future reference

--- src/spotify_utils.py
+++ src/spotify_utils.py
@@ -3,18 +3,18 @@ from bsky_utils import post_now_playing
 
 
 
-def get_now_playing(sp):
-    currentTrack = None
-    track = sp.current_user_playing_track()     # Get track info
-    if track and track['item']:
-        duration = track['item']['duration_ms'] # in milliseconds
-        progress = track['progress_ms']
-        if progress < duration * 0.25:
-            currentTrack = track
-        remainingTime = (duration - progress) / 1000    # in seconds
-        return currentTrack, remainingTime
-    else:
-        return None, 9999
+# def get_now_playing(sp):
+#     currentTrack = None
+#     track = sp.current_user_playing_track()     # Get track info
+#     if track and track['item']:
+#         duration = track['item']['duration_ms'] # in milliseconds
+#         progress = track['progress_ms']
+#         if progress < duration * 0.25:
+#             currentTrack = track
+#         remainingTime = (duration - progress) / 1000    # in seconds
+#         return currentTrack, remainingTime
+#     else:
+#         return None, 9999
 
 # def get_track_info(track):
 #     if track and track['item']:
@@ -26,9 +26,9 @@ def get_now_playing(sp):
 #     else:
 #         return None
 
-def get_last_10_tracks(sp):
-    recent_tracks = sp.current_user_recently_played(limit=10)
+# def get_last_10_tracks(sp):
+#     recent_tracks = sp.current_user_recently_played(limit=10)
 
-    for idx, item in enumerate(recent_tracks['items']):
-        track = item['track']
-        print(f"{idx + 1}: {track['name']} by {track['artists'][0]['name']}")
+#     for idx, item in enumerate(recent_tracks['items']):
+#         track = item['track']
+#         print(f"{idx + 1}: {track['name']} by {track['artists'][0]['name']}")
